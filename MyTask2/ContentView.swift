13f120708741241933f6ce5ccc145f93e6177c26
Pri-Codes <pri.codes@gmail.com>
2023-05-18 16:34:39 +1200

    Initial Commit

---
+++ MyTask2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyTask2
+//
+//  Created by Priscila Oliveira on 18/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyTask2/MyTask2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyTask2App.swift
+//  MyTask2
+//
+//  Created by Priscila Oliveira on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MyTask2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
